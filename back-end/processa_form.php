81b4a398b2a43c83b16c6939fd550048125c4cee
Larissa R. Couto <10105551970@edu.udesc.br>
2023-11-09 20:36:56 -0300

    Processamento do formulário de inscrição

--- back-end/processa_form.php
+++ back-end/processa_form.php
@@ -1,15 +1,13 @@
 <?php
-   print_r($_GET);
-
-    // print_r($_GET);
+// print_r($_GET);
     $nome = $_GET['nome'];
     $idade = $_GET['idade'];
 
-    echo '<h1>Dados de Usuário</1>';
+    echo '<h1>Dados de Usuário</h1>';
     echo '<hr>';
     echo "<p>0 usuário $nome tem $idade anos.</p>";
 
-    echo '<a href="http://localhost/udesc/index.html"><button>Home Page</button></a>';
+    echo '<a href="http://localhost/udesc-larissa/index.html"><button>Home Page</button></a>';
     echo '<button type="button" onclick="javascript:history.back()">Voltar</button>';
     echo '<button onclick="window.location.href=\'https:\\www.udesc.br\'">Vai para Udesc</button>';
 ?>
